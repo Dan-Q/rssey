a9a56a10f3968a6222b28efcd8e1f93d0d86c307
Dan Q <dan@danq.me>
2018-03-27 14:27:32 +0200

    Initial commit.

---
+++ app.js
@@ -0,0 +1,6 @@
+var http = require('http');
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end("hello world!\n");
+});
+server.listen(3000);
